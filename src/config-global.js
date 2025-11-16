8739b1fa6710ffe06c5cd22064524cad17176a35
zak-info <z.benslite@esi-sba.dz>
2025-11-16 20:59:55 +0100

    config

--- src/config-global.js
+++ src/config-global.js
@@ -11,13 +11,10 @@ export const api_version = "v1"
 
 
 // export const HOST_API = 'http://172.105.83.136:8044/api';
-// export const STORAGE_API = 'http://172.105.83.136:8044/storage';
 // export const HOST_API = `http://8.222.151.11:80/api/${api_version}`;
-// export const STORAGE_API = 'http://172.233.43.164:8000/storage';
 // export const HOST_API = 'http://127.0.0.1:8000/api'; 
-// export const STORAGE_API = 'http://127.0.0.1:8000/storage';
-export const HOST_API = `https://be.markium.online/api/${api_version}`;
-export const STORAGE_API = 'https://be.markium.online/storage';
+// export const HOST_API = `https://be.markium.online/api/${api_version}`;
+export const HOST_API = `https://be-test.markium.online/api/${api_version}`;
 export const ASSETS_API = import.meta.env.VITE_ASSETS_API;
 
 export const FIREBASE_API = {
